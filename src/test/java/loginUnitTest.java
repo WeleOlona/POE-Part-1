11fe77b9a1c3e70da880801f0e8b5575b330de2e
WeleOlona <ST10477690@myemeris.edu.za>
2026-04-09 14:43:29 +0200

    eight commit

--- src/test/java/loginUnitTest.java
+++ src/test/java/loginUnitTest.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
 
+import com.mycompany.loginmain.Login;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,6 +15,25 @@ import static org.junit.jupiter.api.Assertions.*;
  *
  * @author lab_services_student
  */
+
 public class loginUnitTest {
-    
+   
+
+    Login login = new Login();
+
+    @Test
+    public void testUsernameFormat() {
+        //  "kyl_1" should be true
+        assertTrue(login.checkUserName("kyl_1"));
+        
+        // "kyle!!!!!!" should be false
+        assertFalse(login.checkUserName("kyle!!!!!!"));
+    }
+
+    @Test
+    public void testPasswordComplexity() {
+        // Test Data: "Ch&&sec@ke99!" should be true
+        assertTrue(login.checkPasswordComplexity("Ch&&sec@ke99!"));
+    }
 }
+
